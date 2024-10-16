ec889855c7647d225da2f1787a9a4883e91fb290
Raegan Beck <raeganbeck@Raegans-MacBook-Pro.local>
2024-10-16 16:08:23 -0600

    allows for user input in weather data

--- src/components/carouselcomponents/FirstCarousel.vue
+++ src/components/carouselcomponents/FirstCarousel.vue
@@ -2,7 +2,14 @@
   <div class="carousel-container">
     <div class="title">Weather Component</div>
     <div class="component" v-if="data">
-      <input type="text" />
+      <div class="input">
+        <input
+          type="text"
+          placeholder="Search a new city"
+          v-model="inputCity"
+        />
+        <button class="submit-button" @click="handleSubmit">submit</button>
+      </div>
       <div class="city">Current City: {{ data.address }}</div>
       <div class="temp">Current Temp: {{ data.currentConditions.temp }}</div>
       <div class="time">
@@ -19,22 +26,31 @@ export default {
   name: "FirstCarousel",
   setup() {
     const data = ref(null);
+    const inputCity = ref("Orem,UT");
 
     const fetchData = async () => {
       const response = await fetch(
-        " https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Orem%2CUT?unitGroup=us&key=XPUF7PB4S4B6P9AD5GWFTGPSH&contentType=json "
+        // ` https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Orem%2CUT?unitGroup=us&key=XPUF7PB4S4B6P9AD5GWFTGPSH&contentType=json `
+        ` https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${inputCity.value}?key=XPUF7PB4S4B6P9AD5GWFTGPSH&contentType=json `
       );
+
       data.value = await response.json();
+    };
+
+    const handleSubmit = () => {
+      fetchData();
+      // inputCity.value = "";
     };
 
     onMounted(() => {
       fetchData();
-      console.log(data.value);
     });
 
     return {
       data,
+      inputCity,
       fetchData,
+      handleSubmit,
       onMounted,
     };
   },
